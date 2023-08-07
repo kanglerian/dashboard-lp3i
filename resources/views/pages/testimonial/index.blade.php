eb1360b189528920bb1d8611165f2a04eeacd951
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-08-07 12:39:13 +0700

    career

--- resources/views/pages/testimonial/index.blade.php
+++ resources/views/pages/testimonial/index.blade.php
@@ -207,6 +207,7 @@
                                     <input type="hidden" name="work" value="{{ $alumni->work }}">
                                     <input type="hidden" name="profession" value="{{ $alumni->profession }}">
                                     <input type="hidden" name="quote" value="{{ $alumni->quote }}">
+                                    <input type="hidden" name="career" value="{{ $alumni->career }}">
                                     <input type="hidden" name="year" value="{{ $alumni->year }}">
                                     <input type="hidden" name="status" value="{{ $alumni->status == 1 ? 0 : 1 }}">
                                     <button role="button" type="submit"
