f399646fc19a4e3c617e095a708c060adc16d28a
kanglerian <kanglerian@gmail.com>
2023-08-24 16:02:27 +0700

    luaran

--- app/Http/Controllers/API/LuaranPkmUPPMAPIController.php
+++ app/Http/Controllers/API/LuaranPkmUPPMAPIController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\DataUPPM;
+use App\Models\LuaranUPPM;
 use Illuminate\Http\Request;
 
 class LuaranPkmUPPMAPIController extends Controller
 {
     public function index()
     {
-        $data = DataUPPM::orderBy('year','desc')->where('type','PKM')->get();
+        $data = LuaranUPPM::orderBy('year','desc')->where('type','PKM')->get();
         return response()->json($data);
     }
 }
