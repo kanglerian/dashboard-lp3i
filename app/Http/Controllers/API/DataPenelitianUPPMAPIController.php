140d29816145026a17f6451f1b881ceb395f4af6
kanglerian <kanglerian@gmail.com>
2023-08-24 14:54:56 +0700

    where

--- app/Http/Controllers/API/DataPenelitianUPPMAPIController.php
+++ app/Http/Controllers/API/DataPenelitianUPPMAPIController.php
@@ -10,7 +10,7 @@ class DataPenelitianUPPMAPIController extends Controller
 {
     public function index()
     {
-        $data = DataUPPM::orderBy('year','desc')->get();
+        $data = DataUPPM::orderBy('year','desc')->where('type','Penelitian')->get();
         return response()->json($data);
     }
 }
--- app/Http/Controllers/API/DataPkmUPPMAPIController.php
+++ app/Http/Controllers/API/DataPkmUPPMAPIController.php
@@ -10,7 +10,7 @@ class DataPkmUPPMAPIController extends Controller
 {
     public function index()
     {
-        $data = DataUPPM::orderBy('year','desc')->get();
+        $data = DataUPPM::orderBy('year','desc')->where('type','PKM')->get();
         return response()->json($data);
     }
 }
